1622c0122c928f9e244b7cf8c6b9ba4c5ea35834
Quazi Zia <qzia07@gmail.com>
2025-11-07 19:22:00 -0800

    wip reset function

--- sudoku_core.asm
+++ sudoku_core.asm
@@ -123,3 +123,78 @@ getCell_error:
 	li $v1, -1
 	jr $ra
 
+#Function D: reset
+reset:
+    addi $sp, $sp, -28
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)
+    sw $s1, 8($sp)
+    sw $s2, 12($sp)
+    sw $s3, 16($sp)
+    sw $s4, 20($sp)
+    sw $s5, 24($sp)
+    move $s0, $a0
+    move $s1, $a1
+    move $s2, $a2
+    bgez $s2, reset_skip_errcheck
+    j reset_clearAll
+    
+reset_skip_errcheck:
+    bgt $s1, 0xF, reset_error
+    beqz $s2, reset_presetOnly
+    j reset_findConflicts
+
+reset_clearAll:
+    li $s3, 0
+reset_clearAll_rowLoop:
+    bge $s3, 9, reset_success
+    li $s4, 0
+reset_clearAll_colLoop:
+    bge $s4, 9, reset_clearAll_nextRow
+    
+    move $a0, $s3
+    move $a1, $s4
+    li $a2, 0
+    li $a3, 0xF0 #white, black
+    jal setCell
+    bltz $v0, reset_error
+    
+    addi $s4, $s4, 1
+    j reset_clearAll_colLoop
+    
+reset_clearAll_nextRow:
+    addi $s3, $s3, 1     # row++
+    j reset_clearAll_rowLoop
+
+reset_presetOnly:
+    #reset to prset colors only
+    #sudoku gui bugged(mars)
+    srl $t0, $s0, 8 #preset
+    andi $t0, $t0, 0xFF
+    andi $t1, $s0, 0xFF  #game
+    
+    li $s3, 0
+reset_preset_rowLoop:
+    bge $s3, 9, reset_success
+    li $s4, 0
+    
+reset_preset_colLoop:
+    bge $s4, 9, reset_preset_nextRow
+    move $a0, $s3
+    move $a1, $s4
+    jal isPresetCell
+    bltz $v0, reset_error
+    bnez $v0, reset_preset_ispreset
+    move $a0, $s3
+    move $a1, $s4
+    jal getCell
+    li $t2, 0xFF
+    beq $v0, $t2, reset_error
+    beqz $v1, reset_preset_nextCell
+    move $a0, $s3
+    move $a1, $s4
+    li $a2, 0
+    move $a3, $t1
+    jal setCell
+    bltz $v0, reset_error
+    j reset_preset_nextCell
