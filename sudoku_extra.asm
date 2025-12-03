3932703fe5866ad0657d75474203215a19b8331d
Quazi Zia <qzia07@gmail.com>
2025-12-02 18:15:00 -0800

    save board function

--- sudoku_extra.asm
+++ sudoku_extra.asm
@@ -108,3 +108,138 @@ saveBoard:
 	sw $ra, 0($sp)
 	sw $s0, 4($sp) #desciptor
 	sw $s1, 8($sp) #playerColors
+	sw $s2, 12($sp) # prest count
+	sw $s3, 16($sp) #ame count
+	sw $s4, 20($sp) # row counter
+	sw $s5, 24($sp) # col counter
+	sw $s6, 28($sp) # preset fg color
+	sw $s7, 32($sp) # game fg color
+	#8byt es for output lines
+	
+	move $t9, $a0 # save filename
+	move $s1, $a1 #save playerColors
+	
+	srl $t0, $s1, 8 #preset color
+	andi $s6, $t0, 0x0F # pfg
+	andi $s7, $s1, 0x0F #gamefg
+	
+	#open file for writing
+	move $a0, $t9
+	li $a1, 1 #write only
+	li $a2, 0 # mode
+	li $v0, 13 # 13 is open file
+	syscall
+	
+	bltz $v0, saveBoard_error
+	move $s0, $v0 #save fil
+	
+	#counters
+	li $s2, 0 #preset counts
+	li $s3, 0 # game count
+	
+	#loop through board
+	li $s4, 0
+saveBoard_row_loop:
+	bge $s4, 9, saveBoard_close
+	li $s5, 0
+	
+saveBoard_col_loop:
+	bge $s5, 9, saveBoard_next_row
+	
+	#cell info
+	move $a0, $s4
+	move $a1, $s5
+	jal getCell
+	li $t0, 0xFF
+	beq $v0, $t0, saveBoard_file_error
+	
+	#1-9
+	beqz $v1, saveBoard_next_col  #skip empty
+	blt $v1, 1, saveBoard_file_error
+	bgt $v1, 9, saveBoard_file_error
+	
+	#preset/gamecell by fg color
+	andi $t1, $v0, 0x0F
+	
+	addi $a1, $sp, 36
+	
+	#0-8
+	addi $t2, $s4, 48
+	sb $t2, 0($a1)
+	
+	#A-I
+	addi $t2, $s5, 65
+	sb $t2, 1($a1)
+	
+	#1-9
+	addi $t2, $v1, 48
+	sb $t2, 2($a1)
+	
+	#P or G
+	beq $t1, $s6, saveBoard_preset_cell
+	beq $t1, $s7, saveBoard_game_cell
+	j saveBoard_file_error
+	
+saveBoard_preset_cell:
+	li $t2, 'P'
+	sb $t2, 3($a1)
+	addi $s2, $s2, 1 #increment
+	j saveBoard_write_line
+	
+saveBoard_game_cell:
+	li $t2, 'G'
+	sb $t2, 3($a1)
+	addi $s3, $s3, 1 #increment
+	
+saveBoard_write_line:
+	li $t2, '\n'
+	sb $t2, 4($a1)
+	
+	move $a0, $s0
+	addi $a1, $sp, 36
+	li $a2, 5
+	li $v0, 15 #write to file 
+	syscall
+	
+	bltz $v0, saveBoard_file_error
+	
+saveBoard_next_col:
+	addi $s5, $s5, 1
+	j saveBoard_col_loop
+	
+saveBoard_next_row:
+	addi $s4, $s4, 1
+	j saveBoard_row_loop
+	
+saveBoard_close:
+	move $a0, $s0
+	li $v0, 16 # close file (16)
+	syscall
+	
+	move $v0, $s2  
+	move $v1, $s3
+	j saveBoard_done
+	
+saveBoard_file_error:
+	move $a0, $s0
+	li $v0, 16
+	syscall
+	
+saveBoard_error:
+	li $v0, -1
+	li $v1, -1
+	
+saveBoard_done:
+	lw $ra, 0($sp)
+	lw $s0, 4($sp)
+	lw $s1, 8($sp)
+	lw $s2, 12($sp)
+	lw $s3, 16($sp)
+	lw $s4, 20($sp)
+	lw $s5, 24($sp)
+	lw $s6, 28($sp)
+	lw $s7, 32($sp)
+	addi $sp, $sp, 52
+	jr $ra
+
+
